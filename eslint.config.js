fa1bd4c7f2fb68234dc7f19be379898c3ac4a8ef
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-04-04 18:38:31 -0500

    fix eslint config

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import { Linter } from "eslint";
+
+/** @type {Linter.Config} */
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
+
+export default config;
