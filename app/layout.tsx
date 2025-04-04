5846775c6282a5b8fedaa44d324f2cc78d38e3e9
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-04-04 18:31:11 -0500

    add eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "@/components/ui/header";
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Local Friend",
+  description: "Local Friend",
+  generator: "Local Friend",
 };
 
 export default function RootLayout({
