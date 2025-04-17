bd47a85b0ab9109706b631aaadb803791d3380bc
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-04-17 12:02:03 -0500

    update root layout

--- src/components/ui/root-layout.tsx
+++ src/components/ui/root-layout.tsx
@@ -2,14 +2,14 @@ import { Outlet } from '@tanstack/react-router'
 import { Footer } from './footer'
 import Header from '@components/ui/header'
 
-export function Layout() {
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header />
-      <main className="mt-18 w-full flex-1">
-        <Outlet />
-      </main>
+      <Outlet />
+      <main className="mt-18 w-full flex-1">{children}</main>
       <Footer />
     </div>
   )
 }
+
